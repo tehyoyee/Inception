d5890f2c6d49c47a2fa9aefb7cb78975b030584f
root <root@tehyoyee.localdomain>
2023-07-30 04:51:17 +0900

    update

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -17,8 +17,8 @@ wp core download --allow-root
 
 mv /var/www/wp-config.php /var/www/wordpress/
 
-wp core install --allow-root --url=${DOMAIN_URL} --title=${WP-TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_ADMIN_EMAIL}
-wp user create ${MARIA_USER} ${WORDPRESS_USER_EMAIL} --user_pass=${MARIA_PW} --role=author --allow-root
+wp core install --allow-root --url=${DOMAIN_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_ADMIN_EMAIL}
+wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_PW} --role=author --allow-root
 wp theme install inspiro --activate --allow-root
 wp plugin install redis-cache --activate --allow-root
 wp plugin update --all --allow-root
