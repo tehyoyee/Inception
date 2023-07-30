4e6372f8b7f878074ad331f992e545bb1e62ed71
root <root@tehyoyee.localdomain>
2023-07-31 00:01:45 +0900

    edit test

--- test/srcs/requirements/wordpress/conf/setup.sh
+++ test/srcs/requirements/wordpress/conf/setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-chown -R www-data /var/www/wordpress
-chmod -R 775 /var/www/wordpress
+# chown -R www-data /var/www/wordpress
+# chmod -R 775 /var/www/wordpress
 
 mkdir -p /run/php/
-touch /run/php/php7.3-fpm.pid
+touch /run/php/php7.4-fpm.pid
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-cd /var/www/wordpress
+cd /var/www/html
 
 wp core download --allow-root
 
-mv /var/www/wp-config.php /var/www/wordpress/
+mv /var/www/wp-config.php /var/www/html/
 
 wp core install --allow-root --url=${DOMAIN_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_ADMIN_EMAIL}
 wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_PW} --role=author --allow-root
@@ -28,4 +28,4 @@ fi
 
 wp redis enable --force --allow-root
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.4 -F
