2066dc3029749607eeb55047b2c41f56f80e1d84
root <root@tehyoyee.localdomain>
2023-08-14 16:10:29 +0900

    ad

--- a/srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ a/srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -5,8 +5,8 @@ chown -R www-data:www-data /var/www/
 if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	sudo -u www-data sh -c " \
 	wp core download --locale=ko_KR && \
-	wp config create --dbname=$V_DB --dbuser=$V_USR --dbpass=$V_PW --dbhost=$V_DB_HOST --dbcharset="utf8" && \
-	wp core install --url=$DOMAIN_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL --skip-email && \
+	wp config create --dbname=$DB --dbuser=$ADMIN --dbpass=$ADMIN_PW --dbhost=$DB_HOST --dbcharset="utf8" && \
+	wp core install --url=$DOMAIN_URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PW --admin_email=$ADMIN_EMAIL --skip-email && \
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD && \
 	wp plugin update --all
 	"
