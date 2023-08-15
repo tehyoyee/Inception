70ef1ffbf3676026a1614e7083d90a32e51d3a08
taehykim <tehyoyee@gmail.com>
2023-08-15 11:11:43 +0900

    b

--- a/srcs/requirements/mariadb/tools/script.sh
+++ a/srcs/requirements/mariadb/tools/script.sh
@@ -6,8 +6,8 @@ service mysql start
 
 
 echo "CREATE DATABASE IF NOT EXISTS wordpressDB ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$taehykim'@'%' IDENTIFIED BY 'tae1234' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON wordpressDB.* TO '$taehykim'@'%' ;" >> db1.sql
+echo "CREATE USER IF NOT EXISTS 'taehykim'@'%' IDENTIFIED BY 'tae1234' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON wordpressDB.* TO 'taehykim'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1234' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
