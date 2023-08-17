4705fe05d97be17a9b16e7a6ae2602a7d22a96f4
taehykim <tehyoyee@gmail.com>
2023-08-17 20:33:42 +0900

    b

--- a/srcs/requirements/wordpress/tools/wp-config.php
+++ a/srcs/requirements/wordpress/tools/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpressDB' );
+define( 'DB_NAME', 'db1' );
 
 /** MySQL database username */
-define( 'DB_USER', 'taehykim' );
+define( 'DB_USER', 'user' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'tae1234' );
+define( 'DB_PASSWORD', 'pwd' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mariadb' );
--- d/srcs/requirements/mariadb/tools/init.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/mariadb-install-db --defaults-file=/etc/my.cnf.d/mariadb-server.cnf
-exec $@
