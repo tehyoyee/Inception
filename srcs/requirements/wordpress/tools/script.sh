8dc3badcd5567109998fd120d3f85c2014bab20f
taehykim <tehyoyee@gmail.com>
2023-08-21 16:45:53 +0900

    final

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p /var/www/html/
-cd /var/www/html/
+mkdir -p /var/www/html
+cd /var/www/html
 
 rm -rf *
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
@@ -10,8 +10,8 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/bin/wp
 
 wp core download --locale=ko_KR --allow-root
-rm /var/www/html/wp/wp-config-sample.php
-mv /wp-config.php /var/www/html/wp/wp-config.php
+rm /var/www/html/wp-config-sample.php
+mv /wp-config.php /var/www/html/wp-config.php
 
 wp core install --url=$DOMAIN_URL/ --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PW --admin_email=$ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
